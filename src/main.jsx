a7238a090ec79d5de5574d59cd265f00cc85d3f9
tomiachaval <tomas.achaval@kaizensoftware.com.ar>
2026-05-16 20:46:06 -0300

    fix: corregir ruta del script principal en index.html para produccion

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // Aquí se cargan tus estilos de Tailwind v4
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
